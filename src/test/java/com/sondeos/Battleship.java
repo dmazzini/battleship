ab9dd6499675d96c1a5e90d4889aa7e836791d8a
dmazzini <dmazzini@gmail.com>
2022-10-29 10:31:40 -0300

    quinto test sunk verde

--- src/test/java/com/sondeos/Battleship.java
+++ src/test/java/com/sondeos/Battleship.java
@@ -2,15 +2,22 @@ package com.sondeos;
 public class Battleship {
     private int[] locations;
 
+    private int hits = 0;
     public void setLocations(int[] locations) {
         this.locations = locations;
     }
 
     public String shot(int userShot) {
-if(locations[0] == userShot || locations[1] == userShot || locations[2] == userShot) {
-            return "hit";
+        String ret = "";
+        if (locations[0] == userShot || locations[1] == userShot || locations[2] == userShot) {
+            hits++;
+            ret = "hit";
+            if (hits == 3) {
+                ret = "sunk";
+            }
         } else {
-            return "miss";
+            ret = "miss";
         }
+        return ret;
     }
 }
