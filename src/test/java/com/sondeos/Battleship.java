fc09dbb4956b69f021dda34d0ef4fa44432b37f3
Dami <dami@sondeos-damian.fibertel.com.ar>
2022-10-29 01:17:41 -0300

    cuarto test cambio locations verde

--- src/test/java/com/sondeos/Battleship.java
+++ src/test/java/com/sondeos/Battleship.java
@@ -1,10 +1,13 @@
 package com.sondeos;
 public class Battleship {
+    private int[] locations;
+
     public void setLocations(int[] locations) {
+        this.locations = locations;
     }
 
     public String shot(int userShot) {
-        if(userShot == 2 || userShot == 3) {
+        if(locations[0] == userShot || locations[1] == userShot || locations[2] == userShot) {
             return "hit";
         } else {
             return "miss";
